935e9c71f86fd91f2f6e892e0a0e1f4089809e26
k.ehrlinger@web.de <ehrlinger@nbw-imp-307.imp.univie.ac.at>
2023-04-14 13:25:37 +0200

    finished multiple sequence alignment, and merged script with consensus script

--- scripts/mre_msa.sh
+++ scripts/mre_msa.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-INPUT_DIR="/groups/bi-marvl/klaus/mre/output/sorted_reads"
-OUTPUT_DIR="/groups/bi-marvl/klaus/mre/output/msa"
-SAMPLE_LIST=$(ls $INPUT_DIR)
-#echo $SAMPLE_LIST
-for FILE in $SAMPLE_LIST
+INPUT_DIR_MSA="/groups/bi-marvl/klaus/mre/output/sorted_reads"
+OUTPUT_DIR_MSA="/groups/bi-marvl/klaus/mre/output/msa"
+OUTPUT_DIR_CONS="/groups/bi-marvl/klaus/mre/output/consensus"
+SAMPLE_LIST_MSA=$(ls $INPUT_DIR_MSA)
+
+rm $OUTPUT_DIR_CONS/*
+touch $OUTPUT_DIR_CONS/placeholder.txt
+rm $OUTPUT_DIR_MSA/*
+touch $OUTPUT_DIR_CONS/placeholder.txt
+
+for FILE in $SAMPLE_LIST_MSA
 do
-    clustalo-1.2.4 --force -i /groups/bi-marvl/klaus/mre/output/sorted_reads/${FILE} -o /groups/bi-marvl/klaus/mre/output/msa/${FILE}_msa.fa
+    FILE_NAME=$(basename ${FILE} .fa)
+    muscle -msf -in ${INPUT_DIR_MSA}/${FILE} -out ${OUTPUT_DIR_MSA}/${FILE_NAME}_msa.msf
+    em_cons -sequence ${OUTPUT_DIR_MSA}/${FILE_NAME}_msa.msf -datafile 'EDNAFULL' -outseq ${OUTPUT_DIR_CONS}/${FILE_NAME}_cons.fa
 done
+
